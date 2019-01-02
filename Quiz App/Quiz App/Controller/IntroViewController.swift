5f77f17ea2162727ed6d24cca74a42246003884c
Chris Rehagen <42623969+ccrwy5@users.noreply.github.com>
2019-01-02 14:32:48 -0600

    Added Intro View/VC

---
+++ Quiz App/Quiz App/Controller/IntroViewController.swift
@@ -0,0 +1,25 @@
+//
+//  IntroViewController.swift
+//  Quiz App
+//
+//  Created by Chris Rehagen on 1/2/19.
+//  Copyright © 2019 Chris Rehagen. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var continueButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        continueButton.layer.cornerRadius = 16
+
+    }
+    
+
+    
+
+}
